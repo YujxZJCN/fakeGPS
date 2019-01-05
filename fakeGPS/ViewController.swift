9bf08c77e8e106d1a47b32cc95d61ae03a425791
俞佳兴 <albertyu@ys-macbook-pro.local>
2019-01-05 14:33:01 +0800

    Initial Commit

---
+++ fakeGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fakeGPS
+//
+//  Created by 俞佳兴 on 2018/12/1.
+//  Copyright © 2018 Albert the Genius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
